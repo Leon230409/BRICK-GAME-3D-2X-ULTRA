a927211ec455a6571eb9f028f94a52c0a075f51e
Dimaric-cool <Dimaric.cool@gmail.com>
2023-12-18 03:57:49 +0300

    Пересмотрел код с учетом класса Game

--- main.py
+++ main.py
@@ -9,30 +9,33 @@ FPS = 60
 PADDLE_WIDTH = 120
 PADDLE_HEIGHT = 10
 BALL_RADIUS = 10
+pygame.font.init()
 LIVES_FONT = pygame.font.SysFont("comicsans", 40)
 LEVEL = 1
+
 
 # Game class
 class Game:
     def __init__(self):
+        self.ball = Ball(WIDTH / 2, HEIGHT - PADDLE_HEIGHT - 5 - BALL_RADIUS, BALL_RADIUS,
+                         gameLevels[LEVEL]["ballColor"])
+        self.paddle = Paddle()
         pygame.init()
         self.win = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("BRICK BREAKER")
         self.clock = pygame.time.Clock()
         self.all_sprites = pygame.sprite.Group()
+        self.bricks = pygame.sprite.Group()
         self.initialize_game()
+        self.LIVES = 3
 
     def initialize_game(self):
         # Initialize game objects
-        self.paddle = Paddle()
-        self.ball = Ball(WIDTH / 2, HEIGHT - PADDLE_HEIGHT - 5 - BALL_RADIUS, BALL_RADIUS, gameLevels[LEVEL]["ballColor"])
-        self.bricks = generate_bricks(LEVEL)
-        self.lives = 3
+        self.bricks = self.generate_bricks()
+        self.LIVES = 3
 
         # Add sprites to the sprite group
         self.all_sprites.add(self.paddle)
-        self.all_sprites.add(self.ball)
-        self.all_sprites.add(self.bricks)
 
         # Load background image
         self.background_image = pygame.image.load(gameLevels[LEVEL]["bckImg"])
@@ -47,22 +50,25 @@ class Game:
             self.paddle.move(-1)
         if keys[pygame.K_d] and self.paddle.rect.x + self.paddle.width < WIDTH:
             self.paddle.move(1)
+        if keys[pygame.K_z]:
+            print("STOP")
         return True
 
     def update_game_state(self):
         self.ball.move()
-        ball_collision(self.ball)
-        ball_paddle_collision(self.ball, self.paddle)
-        ball_flor_collision(self.ball, self.paddle)
+        self.ball_collision(self.ball)
+        self.ball_paddle_collision()
+        self.ball_flor_collision()
+
         for brick in self.bricks:
-            ball_brick_collision(brick, self.ball)
+            self.ball_brick_collision(brick)
             if brick.health <= 0:
                 self.bricks.remove(brick)
 
         if len(self.bricks) <= 0:
             self.load_next_level()
 
-        if self.lives <= 0:
+        if self.LIVES <= 0:
             self.reset_game()
 
     def load_next_level(self):
@@ -73,28 +79,107 @@ class Game:
         self.all_sprites.add(self.paddle)
         self.ball = Ball(WIDTH / 2, HEIGHT - PADDLE_HEIGHT - 5 - BALL_RADIUS, BALL_RADIUS,
                          gameLevels[LEVEL]["ballColor"])
-        self.bricks = generate_bricks(LEVEL)
+        self.bricks = self.generate_bricks()
         self.background_image = pygame.image.load(gameLevels[LEVEL]["bckImg"])
         self.background_image = pygame.transform.scale(self.background_image, (WIDTH, HEIGHT))
-        self.lives = 3
+        self.LIVES = 3
 
     def reset_game(self):
         global LEVEL
-        LEVEL = 0
+        LEVEL = 1
         self.all_sprites.remove(self.paddle)
         self.paddle = Paddle()
         self.all_sprites.add(self.paddle)
-        self.ball = Ball(WIDTH / 2, HEIGHT - PADDLE_HEIGHT - 5 - BALL_RADIUS, BALL_RADIUS, gameLevels[LEVEL]["ballColor"])
-        self.bricks = generate_bricks(LEVEL)
+        self.ball = Ball(WIDTH / 2, HEIGHT - PADDLE_HEIGHT - 5 - BALL_RADIUS, BALL_RADIUS,
+                         gameLevels[LEVEL]["ballColor"])
+        self.bricks = self.generate_bricks()
         self.background_image = pygame.image.load(gameLevels[LEVEL]["bckImg"])
         self.background_image = pygame.transform.scale(self.background_image, (WIDTH, HEIGHT))
-        self.lives = 3
+        self.LIVES = 3
+
+    def generate_bricks(self):
+        brick_sprites = pygame.sprite.Group()
+
+        helpBrick = Brick(-100, -100, 0)
+        brick_width = helpBrick.rect.width
+        brick_height = helpBrick.rect.height
+
+        gap = 5.55
+        bricksMatrix = gameLevels[LEVEL]["brickMap"]
+        for row in range(len(bricksMatrix)):
+            for col in range(len(bricksMatrix[row])):
+                if bricksMatrix[row][col] == 1:
+                    brick = Brick(gap + col * (brick_width + gap), gap + row * (brick_height + gap), 2)
+                    brick_sprites.add(brick)
+                    print(brick.rect.x, brick.rect.y)
+        return brick_sprites
+
+    def ball_collision(self, ball):
+        if ball.x - BALL_RADIUS <= 0:
+            ball.set_position(WIDTH - ball.radius * 2, ball.y)
+        elif ball.x + BALL_RADIUS >= WIDTH:
+            ball.set_position(0 + ball.radius * 2, ball.y)
+
+        if ball.y - BALL_RADIUS <= 0:
+            ball.set_vel(ball.x_vel, ball.y_vel * -1)
+
+    def ball_flor_collision(self):
+        if self.ball.y + BALL_RADIUS >= HEIGHT:
+            self.LIVES -= 1
+            self.ball.set_position(self.paddle.rect.x + self.paddle.width // 2, self.paddle.rect.y - self.ball.radius)
+            self.ball.set_vel(0, -self.ball.VEL)
+
+    def ball_paddle_collision(self):
+        if not ((self.ball.x <= (self.paddle.rect.x + self.paddle.width)) and (self.ball.x >= self.paddle.rect.x)):
+            return
+        if not (self.ball.y + self.ball.radius >= self.paddle.rect.y):
+            return
+
+        distance_to_center = self.ball.x - self.paddle.rect.centerx
+
+        percent_width = distance_to_center / self.paddle.width
+        angle = percent_width * 90
+        angle_radians = math.radians(angle)
+
+        x_vel = math.sin(angle_radians) * self.ball.VEL
+        y_vel = math.cos(angle_radians) * self.ball.VEL
+        self.ball.set_vel(x_vel, y_vel * -1)
+
+    def ball_brick_collision(self, brick):
+        if (brick.rect.y < self.ball.y - self.ball.radius <= brick.rect.bottom) and (
+                brick.rect.x - self.ball.radius < self.ball.x < brick.rect.right + self.ball.radius):
+            print(" удар снизу")
+            brick.hit()
+            self.ball.set_position(self.ball.x, self.ball.y + self.ball.VEL)
+            self.ball.set_vel(self.ball.x_vel, self.ball.y_vel * -1)
+        # удар справа
+        elif (self.ball.x + self.ball.radius >= brick.rect.x) and (
+                self.ball.x + self.ball.radius < brick.rect.right) and (brick.rect.y < self.ball.y < brick.rect.bottom):
+            print(" удар справа")
+            brick.hit()
+            self.ball.set_position(self.ball.x - self.ball.VEL, self.ball.y)
+            self.ball.set_vel(self.ball.x_vel * -1, self.ball.y_vel)
+        elif (self.ball.x - self.ball.radius <= brick.rect.right) and (
+                self.ball.x - self.ball.radius > brick.rect.x) and (brick.rect.y < self.ball.y < brick.rect.bottom):
+            print(" удар слева")
+            brick.hit()
+            self.ball.set_position(self.ball.x + self.ball.VEL, self.ball.y)
+            self.ball.set_vel(self.ball.x_vel * -1, self.ball.y_vel)
+        elif (brick.rect.y <= self.ball.y + self.ball.radius < brick.rect.bottom) and (
+                brick.rect.x - self.ball.radius < self.ball.x < brick.rect.right + self.ball.radius):
+            print(" удар сверху")
+            brick.hit()
+            self.ball.set_position(self.ball.x, self.ball.y - self.ball.VEL)
+            self.ball.set_vel(self.ball.x_vel, self.ball.y_vel * -1)
 
     def draw(self):
         self.win.blit(self.background_image, self.background_image.get_rect())
         self.all_sprites.update()
         self.all_sprites.draw(self.win)
-        lives_text = LIVES_FONT.render(f"HP:{self.lives}", 1, gameLevels[LEVEL]["textColor"])
+        self.bricks.update()
+        self.bricks.draw(self.win)
+        self.ball.draw(self.win)
+        lives_text = LIVES_FONT.render(f"HP:{self.LIVES}", 1, gameLevels[LEVEL]["textColor"])
         self.win.blit(lives_text, (10, HEIGHT - lives_text.get_height() - 10))
         pygame.display.update()
 
@@ -109,6 +194,7 @@ class Game:
         pygame.quit()
         quit()
 
+
 class Paddle(pygame.sprite.Sprite):
     VEL = 5
 
@@ -119,9 +205,6 @@ class Paddle(pygame.sprite.Sprite):
         self.width = self.rect[2]
         self.height = self.rect[3]
         self.rect.center = (WIDTH // 2, HEIGHT - self.height)
-
-    # def draw(self, win):
-    #     pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
 
     def move(self, direction):
         self.rect.x = self.rect.x + self.VEL * direction
@@ -159,18 +242,13 @@ class Brick(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.health = health
         self.images = []
-        self.numDir = random.choice(range(1,4))
-        self.images.append(pygame.image.load(f'images/bt{self.numDir}/bt{self.numDir}_1.png'))
-        self.images.append(pygame.image.load(f'images/bt{self.numDir}/bt{self.numDir}_2.png'))
+        self.numDir = random.choice(range(1, 4))
+        self.images.extend([pygame.image.load(f'images/bt{self.numDir}/bt{self.numDir}_1.png'),
+                            pygame.image.load(f'images/bt{self.numDir}/bt{self.numDir}_2.png')])
         self.imageIndex = 0
         self.image = self.images[self.imageIndex]
         self.rect = self.image.get_rect()
-        self.width = self.rect[2]
-        self.height = self.rect[3]
         self.rect.topleft = (x, y)
-
-    # def draw(self, win):
-        # pygame.draw.rect(win, (self.x, self.y, self.width, self.height))
 
     def hit(self):
         self.health -= 1
@@ -178,182 +256,6 @@ class Brick(pygame.sprite.Sprite):
         if self.imageIndex < len(self.images):
             self.image = self.images[self.imageIndex]
 
-
-# def draw(win, paddle, ball, bricks, lives, back, sprites):
-#     # win.fill("white")
-#     win.blit(back, back.get_rect())
-#     # paddle.draw(win)
-#
-#     sprites.update()
-#     sprites.draw(win)
-#
-#     bricks.update()
-#     bricks.draw(win)
-#
-#     ball.draw(win)
-#
-#     # for brick in bricks:
-#     #     brick.draw(win)
-#
-#     lives_text = LIVES_FONT.render(f"HP:{lives}", 1, gameLevels[LEVEL]["textColor"])
-#     win.blit(lives_text, (10, HEIGHT - lives_text.get_height() - 10))
-#
-#     pygame.display.update()
-
-
-def ball_collision(ball):
-    if ball.x - BALL_RADIUS <= 0:
-        ball.set_position(WIDTH - ball.radius*2, ball.y)
-    elif ball.x + BALL_RADIUS >= WIDTH:
-        ball.set_position(0 + ball.radius*2, ball.y)
-
-    if ball.y - BALL_RADIUS <= 0:
-        ball.set_vel(ball.x_vel, ball.y_vel * -1)
-
-
-def ball_flor_collision(ball, paddle):
-    global lives
-    if ball.y + BALL_RADIUS >= HEIGHT:
-        lives -= 1
-        ball.set_position(paddle.rect.x + paddle.width // 2, paddle.rect.y - ball.radius)
-        ball.set_vel(0, -ball.VEL)
-
-
-def ball_paddle_collision(ball, paddle):
-    if not (ball.x <= paddle.rect.x + paddle.width and ball.x >= paddle.rect.x):
-        return
-    if not (ball.y + ball.radius >= paddle.rect.y):
-        return
-
-    distance_to_center = ball.x - paddle.rect.centerx
-
-    percent_width = distance_to_center / paddle.width
-    angle = percent_width * 90
-    angle_radians = math.radians(angle)
-
-    x_vel = math.sin(angle_radians) * ball.VEL
-    y_vel = math.cos(angle_radians) * ball.VEL
-    ball.set_vel(x_vel, y_vel * -1)
-
-
-def ball_brick_collision(brick, ball):
-    if (brick.rect.y < ball.y - ball.radius <= brick.rect.y + brick.height) and (
-            brick.rect.x - ball.radius < ball.x < brick.rect.x + brick.width + ball.radius):
-        print(" удар снизу")
-        brick.hit()
-        ball.set_position(ball.x, ball.y + ball.VEL)
-        ball.set_vel(ball.x_vel, ball.y_vel * -1)
-        return True
-    # удар справа
-    if (ball.x + ball.radius >= brick.rect.x) and (ball.x + ball.radius < brick.rect.x + brick.width) and (
-            brick.rect.y < ball.y < brick.rect.y + brick.height):
-        print(" удар справа")
-        brick.hit()
-        ball.set_vel(ball.x_vel * -1, ball.y_vel)
-        return True
-    if (ball.x - ball.radius <= brick.rect.x + brick.width) and (ball.x - ball.radius > brick.rect.x) and (
-            brick.rect.y < ball.y < brick.rect.y + brick.height):
-        print(" удар слева")
-        brick.hit()
-        ball.set_vel(ball.x_vel * -1, ball.y_vel)
-        return True
-    if (brick.rect.y <= ball.y + ball.radius < brick.rect.y + brick.height) and (
-            brick.rect.x - ball.radius < ball.x < brick.rect.x + brick.width + ball.radius):
-        print(" удар сверху")
-        brick.hit()
-        ball.set_position(ball.x, ball.y - ball.VEL)
-        ball.set_vel(ball.x_vel, ball.y_vel * -1)
-        return True
-
-    return False
-
-
-def generate_bricks(level):
-    brick_sprites = pygame.sprite.Group()
-
-    helpBrick = Brick(-100, -100, 0)
-    brick_width = helpBrick.width
-    brick_height = helpBrick.height
-
-    gap = 5.55
-    bricksMatrix = gameLevels[level]["brickMap"]
-    for row in range(len(bricksMatrix)):
-        for col in range(len(bricksMatrix[row])):
-            if bricksMatrix[row][col] == 1:
-                brick = Brick(gap + col * (brick_width + gap), gap + row * (brick_height + gap), 2)
-                brick_sprites.add(brick)
-                print(brick.rect.x, brick.rect.y)
-    return brick_sprites
-
-lives = 3
-
-# def main():
-#     global lives, LEVEL, background_image
-#     clock = pygame.time.Clock()
-#
-#     paddle = Paddle()
-#     all_sprites.add(paddle)
-#
-#
-#     ball = Ball(WIDTH / 2, HEIGHT - PADDLE_HEIGHT - 5 - BALL_RADIUS, BALL_RADIUS, gameLevels[LEVEL]["ballColor"])
-#     bricks = generate_bricks(LEVEL)
-#     run = True
-#     while run:
-#         clock.tick(FPS)
-#
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 run = False
-#                 break
-#
-#         keys = pygame.key.get_pressed()
-#         if keys[pygame.K_a] and paddle.rect.x > 0:
-#             paddle.move(-1)
-#         if keys[pygame.K_d] and paddle.rect.x + paddle.width < WIDTH:
-#             paddle.move(1)
-#         ball.move()
-#         ball_collision(ball)
-#         ball_paddle_collision(ball, paddle)
-#         ball_flor_collision(ball, paddle)
-#         for brick in bricks:
-#             ball_brick_collision(brick, ball)
-#             if brick.health <= 0:
-#                 bricks.remove(brick)
-#
-#         if len(bricks) <= 0:
-#             LEVEL += 1
-#             all_sprites.remove(paddle)
-#             paddle = Paddle()
-#             all_sprites.add(paddle)
-#             ball = Ball(WIDTH / 2, HEIGHT - PADDLE_HEIGHT - 5 - BALL_RADIUS, BALL_RADIUS,
-#                         gameLevels[LEVEL]["ballColor"])
-#             bricks = generate_bricks(LEVEL)
-#             background_image = pygame.image.load(gameLevels[LEVEL]["bckImg"])
-#             background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
-#             lives = 3
-#
-#         if lives <= 0:
-#             LEVEL = 0
-#             all_sprites.remove(paddle)
-#             paddle = Paddle()
-#             all_sprites.add(paddle)
-#             ball = Ball(WIDTH / 2, HEIGHT - PADDLE_HEIGHT - 5 - BALL_RADIUS, BALL_RADIUS, gameLevels[LEVEL]["ballColor"])
-#             bricks = generate_bricks(LEVEL)
-#             background_image = pygame.image.load(gameLevels[LEVEL]["bckImg"])
-#             background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
-#             lives = 3
-#
-#             lost_text = LIVES_FONT.render("HAHAHA YOU LOST!!!", 1, "red")
-#             win.blit(lost_text, (WIDTH / 2 - lost_text.get_width() / 2, HEIGHT / 2 - lost_text.get_height() / 2))
-#             pygame.display.update()
-#             pygame.time.delay(5000)
-#         draw(win, paddle, ball, bricks, lives, background_image, all_sprites)
-#     pygame.quit()
-#     quit()
-#
-#
-# if __name__ == "__main__":
-#     main()
 
 if __name__ == "__main__":
     game = Game()
